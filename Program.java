16e69ee6ed3fa8aef9d040bac16e13632e5d2195
Pedro Henrique Boldori Bier <124646793+Pedrohbbier@users.noreply.github.com>
2024-03-25 14:32:06 -0300

    feat: Better way to execute exercise

--- Program.java
+++ Program.java
@@ -1,24 +1,72 @@
 public class Program {
     public static void main(String[] args) {
-        //Ex001.execute();
-        //Ex002.execute();
-        //Ex003.execute();
-        //Ex004.execute();
-        //Ex005.execute();
-        //Ex006.execute();
-        //Ex007.execute();
-        //Ex008.execute();
-        //Ex009.execute();
-        //Ex010.execute();
-        //Ex011.execute();
-        //Ex012.execute();
-        //Ex013.execute();
-        //Ex014.execute();
-        //Ex015.execute();
-        //Ex016.execute();
-        //Ex017.execute();
-        //Ex018.execute();
-        //Ex019.execute();
-        Ex020.execute();
+
+        int ex = Prompt.lerInteiro("Qual exercício deseja executar? ");
+
+        switch (ex) {
+            case 1:
+                Ex001.execute();
+                break;
+            case 2:
+                Ex002.execute();
+                break;
+            case 3:
+                Ex003.execute();
+                break;
+            case 4:
+                Ex004.execute();
+                break;
+            case 5:
+                Ex005.execute();
+                break;
+            case 6:
+                Ex006.execute();
+                break;
+            case 7:
+                Ex007.execute();
+                break;
+            case 8:
+                Ex008.execute();
+                break;
+            case 9:
+                Ex009.execute();
+                break;
+            case 10:
+                Ex010.execute();
+                break;
+            case 11:    
+                Ex011.execute();
+                break;
+            case 12:
+                Ex012.execute();
+                break;
+            case 13:
+                Ex013.execute();
+                break;
+            case 14:
+                Ex014.execute();
+                break;
+            case 15:
+                Ex015.execute();
+                break;
+            case 16:
+                Ex016.execute();
+                break;
+            case 17:
+                Ex017.execute();
+                break;
+            case 18:
+                Ex018.execute();
+                break;
+            case 19:
+                Ex019.execute();
+                break;
+            case 20:    
+                Ex020.execute();
+                break;
+            default:
+                Prompt.imprimir("Exercício inválido , digite 1 para o exercicio 1 e assim por diante ......");
+                break;
+        }
     }
 }
